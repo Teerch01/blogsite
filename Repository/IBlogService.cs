8f1f4c9a0e7958b2bc3f44d2a54e1b1adf7f8075
Teerch <oyundoyintemitayo51@gmail.com>
2024-09-23 11:13:48 +0100

    added jwt autentication

--- Repository/IBlogService.cs
+++ Repository/IBlogService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using blogsite.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,20 +6,21 @@ namespace blogsite.Repository;
 
 public interface IBlogService
 {
-    Task<IEnumerable<User>> GetUsersAsync();
-    Task<User> GetUserByUserNameAsync(string username);
-    Task<IEnumerable<Posts>> GetPostsOfUserById(Guid id);
-    Task<User> CreateUserAsync([FromBody] string firstname, string lastname, string username, string password, string email);
-    Task<bool> UserAlreadyExists(string username);
-    Task<bool> EmailAlreadyExists(string email);
-    Task<User> AutenticateUserAsync(string usernameoremail, string password);
-    Task DeleteUserAsync(Guid id);
-    Task CreatePostAsync([FromBody] string title, string content, Guid userid, string username, string imageurl);
-    Task<Posts> GetPostByIdAsync(int postId);
-    Task<IEnumerable<Posts>> GetPostsAsync();
-    Task<IEnumerable<Posts>> Search(string query);
-    Task<Posts> EditPostAsync(int postId, string title, string content, string imageurl);
-    Task DeletePostAsync(int postid);
-    Task LikePost(int postid, Guid userid);
-    Task<bool> HasUserLikedPost(int postid, Guid userid);
+	Task<IEnumerable<User>> GetUsersAsync();
+	Task<User> GetUserByUserNameAsync(string username);
+	Task<IEnumerable<Posts>> GetPostsOfUserById(Guid id);
+	Task<User> CreateUserAsync([FromBody] string firstname, string lastname, string username, string password, string email);
+	Task<bool> UserAlreadyExists(string username);
+	Task<bool> EmailAlreadyExists(string email);
+	Task<User> AutenticateUserAsync(string usernameoremail, string password);
+	Task DeleteUserAsync(Guid id);
+	Task CreatePostAsync([FromBody] string title, string content, Guid userid, string username, string imageurl);
+	Task<Posts> GetPostByIdAsync(int postId);
+	Task<IEnumerable<Posts>> GetPostsAsync();
+	Task<IEnumerable<Posts>> Search(string query);
+	Task<Posts> EditPostAsync(int postId, string title, string content, string imageurl);
+	Task DeletePostAsync(int postid);
+	Task LikePost(int postid, Guid userid);
+	Task<bool> HasUserLikedPost(int postid, Guid userid);
+	string GenerateJwtToken(IEnumerable<Claim> claims, IConfiguration configuration);
 }
